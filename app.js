100658b62d1905d1c4aee90d1599c61451a69d28
Akitetsuk <m.feijoo.1995@gmail.com>
2022-04-06 12:38:30 +0200

    done

--- app.js
+++ app.js
@@ -8,15 +8,27 @@ Vue.createApp({
             showTextLength: '',
         }
     },
+    mounted() {
+        console.log("Ya me han renderizado!");
+
+        // busca el atributo ref="title"; obtiene el elemento del DOM
+        this.$refs.title.style.backgroundColor = "red";
+        this.$refs.title.textContent = "RENDERIZADO!! Gracias al hook Mounted";
+    },
+    updated() {
+        console.log("Ha cambiado el DOM Virtual!! Te he actualizado el DOM que ves en el navegador.");
+    },
     methods: {
         saludar(nombre) {
             return 'Alo!!!!!' + nombre;
         },
         calculate() {
             this.showTextLength = this.textInput;
+
         },
 
     },
+    // Las computed properties NO deberían modificar el estado
     computed: {
         setColorNumber() {
             // una función que está calculando un estado a partir de otro
@@ -28,6 +40,10 @@ Vue.createApp({
         },
         getTextLength() {
             return this.showTextLength.length;
+        },
+        getArrayFromText() {
+            // me crea un array con tantos elementos como carácteres tiene el string
+            return this.showTextLength.split('')
         }
     }
 
